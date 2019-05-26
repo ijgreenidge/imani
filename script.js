f8523822401376fd4bd65c49e8a8651bc700d2c9
Imani Greenidge <igreenidge2018@bklawtech.com>
2019-05-26 22:50:28 +0000

    added js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".coding").hover(function(){
+        $(".coding-text").toggle();
+    });
+    
+    $(".mbm").hover(function(){
+        $(".mbm-text").toggle();
+    });
+    
+    $(".experience").hover(function(){
+        $(".experience-text").toggle();
+    });
+});
